85a4eaf53e3483df0e3b9e51421edd2d382864f1
Chris <chrislee0419@gmail.com>
2020-12-18 00:19:52 +0000

    initial commit

---
+++ hellosFilters/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace hellosFilters
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger, Config conf)
+        {
+            Instance = this;
+            Plugin.Log = logger;
+            Plugin.Log?.Debug("Logger initialized.");
+
+            PluginConfig.Instance = conf.Generated<PluginConfig>();
+        }
+
+
+        [OnEnable]
+        public void OnEnable()
+        {
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+        }
+    }
+}
