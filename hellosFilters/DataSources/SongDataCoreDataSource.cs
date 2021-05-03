6b74ecb2703c6f8a331bcc1d2fa47aa47b3f7609
Chris <chrislee0419@gmail.com>
2021-05-03 02:17:02 +0100

    process SongDataCore using ThreadPool
    - move async to fallback method
    - faster by about a second (~8500ms to ~7500ms)

--- hellosFilters/DataSources/SongDataCoreDataSource.cs
+++ hellosFilters/DataSources/SongDataCoreDataSource.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Zenject;
 using IPA.Utilities.Async;
@@ -14,7 +15,7 @@ namespace HUIFilters.DataSources
 {
     internal class SongDataCoreDataSource : IInitializable, IDisposable, IBeatmapDataSource, IScoreSaberDataSource, IBeatSaverDataSource
     {
-        public bool IsDataAvailable => _beatmapData.Count > 0 || _scoreSaberData.Count > 0 || _beatSaverData.Count > 0;
+        public bool IsDataAvailable => !_isProcessingData && (_beatmapData.Count > 0 || _scoreSaberData.Count > 0 || _beatSaverData.Count > 0);
 
         private Dictionary<string, BeatmapMetaData> _beatmapData = new Dictionary<string, BeatmapMetaData>(StringComparer.InvariantCultureIgnoreCase);
         private ReadOnlyDictionary<string, BeatmapMetaData> _readOnlyBeatmapData = null;
@@ -52,6 +53,8 @@ namespace HUIFilters.DataSources
             }
         }
 
+        private bool _isProcessingData = false;
+
         public void Initialize()
         {
             const int WaitTime = 1000;
@@ -75,7 +78,7 @@ namespace HUIFilters.DataSources
                 SDCPlugin.Songs.OnDataFinishedProcessing += OnSongDataCoreFinishedProcessing;
 
                 if (SDCPlugin.Songs.IsDataAvailable())
-                    await ProcessData();
+                    await StartProcessingDataAsync();
             });
         }
 
@@ -108,19 +111,81 @@ namespace HUIFilters.DataSources
                     await Task.Delay(1000);
 
                 if (SDCPlugin.Songs.IsDataAvailable())
-                    await ProcessData();
+                    await StartProcessingDataAsync();
                 else
                     Plugin.Log.Error("Unable to process data from SongDataCore (data is not available)");
             });
         }
 
-        private async Task ProcessData()
+        private async Task StartProcessingDataAsync()
         {
             Plugin.Log.Info("Processing data from SongDataCore");
             Stopwatch sw = Stopwatch.StartNew();
 
+            try
+            {
+                _isProcessingData = true;
+
+                ThreadPool.QueueUserWorkItem(ProcessDataThread);
+
+                const int PollTimeMilliseconds = 100;
+                while (_isProcessingData)
+                    await Task.Delay(PollTimeMilliseconds);
+            }
+            catch (NotSupportedException)
+            {
+                Plugin.Log.Warn("Unable to process SongDataCore data using ThreadPool, falling back to async loading");
+
+                await ProcessDataAsync();
+            }
+
+            sw.Stop();
+            Plugin.Log.Info($"Finished processing data from SongDataCore in {sw.ElapsedMilliseconds}ms");
+        }
+
+        private void ProcessDataThread(object state)
+        {
+            foreach (var (hash, song) in SDCPlugin.Songs.Data.Songs)
+            {
+                var beatmapCharacteristics = new Dictionary<string, Dictionary<BeatmapDifficulty, BeatmapMetaData.DifficultyData>>();
+                var scoreSabercharacteristics = new Dictionary<string, Dictionary<BeatmapDifficulty, ScoreSaberData.DifficultyData>>();
+
+                foreach (var (characteristic, difficultiesData) in song.characteristics)
+                {
+                    var beatmapDifficulties = new Dictionary<BeatmapDifficulty, BeatmapMetaData.DifficultyData>();
+                    var scoreSaberDifficulties = new Dictionary<BeatmapDifficulty, ScoreSaberData.DifficultyData>();
+
+                    foreach (var difficulty in difficultiesData.Values)
+                    {
+                        BeatmapDifficulty diffEnum = StringToBeatmapDifficulty(difficulty.diff);
+
+                        beatmapDifficulties.Add(
+                            diffEnum,
+                            new BeatmapMetaData.DifficultyData(difficulty.njs, difficulty.nts, difficulty.bmb, difficulty.obs));
+
+                        scoreSaberDifficulties.Add(
+                            diffEnum,
+                            new ScoreSaberData.DifficultyData(difficulty.star, difficulty.pp));
+                    }
+
+                    string characteristicSerializedName = BeatStarCharacteristicsToSerializedName(characteristic);
+                    beatmapCharacteristics.Add(characteristicSerializedName, beatmapDifficulties);
+                    scoreSabercharacteristics.Add(characteristicSerializedName, scoreSaberDifficulties);
+                }
+
+                _beatmapData.Add(hash, new BeatmapMetaData(beatmapCharacteristics, song.bpm));
+                _scoreSaberData.Add(hash, new ScoreSaberData(scoreSabercharacteristics));
+                _beatSaverData.Add(hash, new BeatSaverData(song.downloadCount, song.upVotes, song.downVotes, song.heat, song.rating));
+            }
+
+            _isProcessingData = false;
+        }
+
+        private async Task ProcessDataAsync()
+        {
             const int WorkTimeLimitMilliseconds = 5;
             Stopwatch workLimitStopwatch = Stopwatch.StartNew();
+
             foreach (var (hash, song) in SDCPlugin.Songs.Data.Songs)
             {
                 var beatmapCharacteristics = new Dictionary<string, Dictionary<BeatmapDifficulty, BeatmapMetaData.DifficultyData>>();
@@ -161,22 +226,16 @@ namespace HUIFilters.DataSources
             }
 
             workLimitStopwatch.Stop();
-            sw.Stop();
-            Plugin.Log.Info($"Finished processing data from SongDataCore in {sw.ElapsedMilliseconds}ms");
+
+            _isProcessingData = false;
         }
 
         private static BeatmapDifficulty StringToBeatmapDifficulty(string str)
         {
-            if (str == "Easy")
-                return BeatmapDifficulty.Easy;
-            else if (str == "Normal")
-                return BeatmapDifficulty.Normal;
-            else if (str == "Hard")
-                return BeatmapDifficulty.Hard;
-            else if (str == "Expert")
-                return BeatmapDifficulty.Expert;
-            else if (str == "Expert+")
+            if (str == "Expert+")
                 return BeatmapDifficulty.ExpertPlus;
+            else if (Enum.TryParse(str, out BeatmapDifficulty diff))
+                return diff;
 
             Plugin.Log.DebugOnly($"Unable to parse difficulty string from SongDataCore data to BeatmapDifficulty ({str})");
             return BeatmapDifficulty.Easy;
